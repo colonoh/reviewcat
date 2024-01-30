735b67c14bb1a40aeb0e7e06be9c2086093b2067
Steve Warner <steve.j.warner@gmail.com>
2024-01-30 00:08:57 -0800

    Working(?) checkpoint

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from random import randrange, sample
-from typing import Union
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
@@ -30,64 +28,16 @@ def read_root(request: Request):
                           evacuation_guidelines=["Evac 1"])
     # print(condition)
 
-    # pick some of the symptoms (but keep all of them)
-    selected_symptoms: List[str] = []
-    vitals_to_modify: List[str] = []  # type: Union...
-
-    # go through the symptoms in a random order so if there are conflicts, the first one doesn't always get picked over the later ones
-    for symptom in sample(condition.symptoms, k=len(condition.symptoms)):
-        if randrange(100) <= symptom.frequency:  # pick a number 0..100, and if it is less than the frequency, select this symptom
-            # check to see if any vitals this symptom has were already modified/conflict e.g. heart rate can't be both rapid AND slow
-            vital_already_modified = False
-            for vital in symptom.vitals:
-                if type(vital) in vitals_to_modify:  # type being something like HEART_RATE
-                    vital_already_modified = True
-                else:
-                    vitals_to_modify.append(type(vital))
-            if not vital_already_modified:
-                selected_symptoms.append(symptom.name)
-
+    selected_symptoms = condition.get_symptoms()
     print(selected_symptoms)
-    print(vitals_to_modify)
 
     # create a baseline patient
     patient = PatientVitals()
-    for symptom in condition.symptoms:
-        if symptom.name in selected_symptoms:
-            # overwrite the patient's default vitals with new ones
-            for vital in symptom.vitals:
-                if vital.name in LEVEL_OF_RESPONSIVENESS.__members__:
-                    patient.level_of_responsiveness = vital.value
-                elif vital.name in HEART_RATE.__members__:
-                    patient.heart_rate = vital.value
-                elif vital.name in HEART_STRENGTH.__members__:
-                    patient.heart_strength = vital.value
-                elif vital.name in HEART_RHYTHM.__members__:
-                    patient.heart_rhythm = vital.value
-                elif vital.name in RESPIRATORY_RATE.__members__:
-                    patient.respiratory_rate = vital.value
-                elif vital.name in RESPIRATORY_RHYTHM.__members__:
-                    patient.respiratory_rhythm = vital.value
-                elif vital.name in RESPIRATORY_EFFORT.__members__:
-                    patient.respiratory_effort = vital.value
-                elif vital.name in SKIN_COLOR.__members__:
-                    patient.skin_color = vital.value
-                elif vital.name in SKIN_TEMPERATURE.__members__:
-                    patient.skin_temperature = vital.value
-                elif vital.name in SKIN_MOISTURE.__members__:
-                    patient.skin_moisture = vital.value
-                elif vital.name in BODY_TEMPERATURE.__members__:
-                    patient.body_temperature = vital.value
-                elif vital.name in PUPILS.__members__:
-                    patient.pupils = vital.value
-                elif vital.name in BLOOD_PRESSURE.__members__:
-                    patient.blood_pressure = vital.value
-                else:
-                    raise ValueError(f"Don't know what vital {vital.name} is!")
+    patient.modify_vitals(selected_symptoms)
 
     # return the select symptoms and patient data
     for s in selected_symptoms:
-        print(s)
+        print(s.name)
     print(patient)
 
     # return all the symptoms, treatments, evac guidelines
@@ -95,6 +45,6 @@ def read_root(request: Request):
 
 
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"msg": "Hello world", "patient": patient}
+        request=request, name="index.html", context={"patient": patient}
     )
 
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from random import randrange, sample
 from typing import List, Union
 
 from pydantic import BaseModel
@@ -30,8 +31,32 @@ class Condition(BaseModel):
     evacuation_guidelines: List[str]
     # references: List[str] = []
 
+    def get_symptoms(self) -> List[Symptom]:
+        """
+        Randomly choose some of the symptoms, based on frequency.  Do not pick symptoms with conflicting vital effects.
+        """
+        selected_symptoms: List[Symptom] = []
+        vitals_to_modify: List[str] = []
+
+        # go through the symptoms in a random order so if there are conflicts, the first one doesn't always get picked over the later ones
+        for symptom in sample(self.symptoms, k=len(self.symptoms)):
+            if randrange(100) <= symptom.frequency:  # pick a number 0..100, and if it is less than the frequency, select this symptom
+                # check to see if any vitals this symptom has were already modified/conflict e.g. heart rate can't be both rapid AND slow
+                vital_already_modified = False
+                for vital in symptom.vitals:
+                    if type(vital) in vitals_to_modify:  # type being something like HEART_RATE
+                        vital_already_modified = True
+                    else:
+                        vitals_to_modify.append(type(vital))
+                if not vital_already_modified:
+                    selected_symptoms.append(symptom)
+        return selected_symptoms
+
 
 class PatientVitals(BaseModel):
+    """
+    A set of values representing normal levels.
+    """
     level_of_responsiveness: str = LEVEL_OF_RESPONSIVENESS.AOx4.value
     heart_rate: int = HEART_RATE.NORMAL.value
     heart_strength: str = HEART_STRENGTH.STRONG.value
@@ -46,3 +71,38 @@ class PatientVitals(BaseModel):
     pupils: str = PUPILS.PERRL.value
     blood_pressure: str = BLOOD_PRESSURE.NORMAL.value
 
+    def modify_vitals(self, symptoms: List[Symptom]):
+        """
+        For any symptoms given which modify vitals, do the modification.
+        """
+        for symptom in symptoms:
+            # overwrite the patient's default vitals with new ones
+            for vital in symptom.vitals:
+                if vital.name in LEVEL_OF_RESPONSIVENESS.__members__:
+                    self.level_of_responsiveness = vital.value
+                elif vital.name in HEART_RATE.__members__:
+                    self.heart_rate = vital.value
+                elif vital.name in HEART_STRENGTH.__members__:
+                    self.heart_strength = vital.value
+                elif vital.name in HEART_RHYTHM.__members__:
+                    self.heart_rhythm = vital.value
+                elif vital.name in RESPIRATORY_RATE.__members__:
+                    self.respiratory_rate = vital.value
+                elif vital.name in RESPIRATORY_RHYTHM.__members__:
+                    self.respiratory_rhythm = vital.value
+                elif vital.name in RESPIRATORY_EFFORT.__members__:
+                    self.respiratory_effort = vital.value
+                elif vital.name in SKIN_COLOR.__members__:
+                    self.skin_color = vital.value
+                elif vital.name in SKIN_TEMPERATURE.__members__:
+                    self.skin_temperature = vital.value
+                elif vital.name in SKIN_MOISTURE.__members__:
+                    self.skin_moisture = vital.value
+                elif vital.name in BODY_TEMPERATURE.__members__:
+                    self.body_temperature = vital.value
+                elif vital.name in PUPILS.__members__:
+                    self.pupils = vital.value
+                elif vital.name in BLOOD_PRESSURE.__members__:
+                    self.blood_pressure = vital.value
+                else:
+                    raise ValueError(f"Don't know what vital {vital.name} is!")
